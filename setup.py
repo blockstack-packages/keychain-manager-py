dd4f67b6032381339ab1248af4ce9ff966029500
Jude Nelson <judecn@gmail.com>
2017-04-20 18:33:28 -0400

    use keylib's encoding methods instead of bitcoin

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ setup(
     url='https://github.com/blockstack/keychain-manager-py',
     license='MIT',
     author='Blockstack Developers',
-    author_email='hello@onename.com',
+    author_email='support@blockstack.com',
     description="""Library for BIP32 hierarchical deterministic keychains / wallets.""",
     keywords='bitcoin blockchain bip32 HD hierarchical deterministic keychain wallet',
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
         'bitmerchant>=0.1.8',
-        'bitcoin>=1.1.42'
+        'keylib>=0.1.0',
     ],
     classifiers=[
         'Intended Audience :: Developers',
